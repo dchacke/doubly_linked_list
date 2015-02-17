fc9fb359b171e03f9dd616a6bf0937e7652668c8
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-02-16 20:21:16 -0800

    Implement Node

---
+++ lib/node.rb
@@ -0,0 +1,9 @@
+class Node
+  attr_reader :value, :previous_node, :next_node
+
+  def initialize(value, previous_node = nil, next_node = nil)
+    @value = value
+    @previous_node = previous_node
+    @next_node = next_node
+  end
+end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,29 @@
 require 'spec_helper'
+
+RSpec.describe Node do
+  let!(:previous_node) { Node.new('foo') }
+  let!(:next_node) { Node.new('foo') }
+  let!(:node) { Node.new('foo', previous_node, next_node) }
+
+  describe 'Initialization' do
+    it 'assigns a value' do
+      expect(node.value).to eq 'foo'
+    end
+
+    it 'assigns nil to the previous_node' do
+      expect(node.previous_node).to eq previous_node
+    end
+
+    it 'assigns nil to the next_node' do
+      expect(node.next_node).to eq next_node
+    end
+
+    it 'assigns nil to the next_node by default' do
+      expect(previous_node.next_node).to be_nil
+    end
+
+    it 'assigns nil to the previous_node by default' do
+      expect(previous_node.previous_node).to be_nil
+    end
+  end
+end
